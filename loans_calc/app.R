8cccb27fc39e7f3ef140ff3327303e2ac254e11e
JMaddalena <JMaddalena@comverge.com>
2018-07-27 09:01:29 -0600

    loan app up and under loan_calc name

---
+++ loans_calc/app.R
@@ -0,0 +1,286 @@
+library(shiny)
+library(tidyverse)
+source("source.R")
+
+x <- list(salliemae = list(name = "Sample Loan 1", balance = 15000, int = 0.0445,
+                           min_pay = 150, fixed = F),
+          navient1 = list(name = "Sample Loan 2", balance = 2400, int = 0.0535,
+                          min_pay = 45, fixed = T),
+          navient2 = list(name = "Sample Loan 3", balance = 3800, int = 0.0315,
+                          min_pay = 50, fixed = T),
+          navient3 = list(name = "Sample Loan 4", balance = 1200, int = 0.0655,
+                          min_pay = 25, fixed = T),
+          navient4 = list(name = "Sample Loan 5", balance = 4000, int = 0.0655,
+                          min_pay = 95, fixed = T))
+
+word_num <- function(word, i){
+  sprintf("%s%s", word, i)
+}
+
+server <- function(input, output){
+  
+  observeEvent(input$submit, {
+    print("Processing options")
+  })
+  
+  counter <- reactiveValues(n = 1)
+  
+  #Track the number of input boxes previously
+  prevcount <- reactiveValues(n = 1)
+  
+  observeEvent(input$add_loan, {
+    if(input$fillin) counter$n <- min(length(x), counter$n + 1)
+    else counter$n <- counter$n + 1
+    prevcount$n <- counter$n - 1
+  })
+  
+  observeEvent(input$minus_loan, {
+    counter$n <- counter$n - 1
+    prevcount$n <- counter$n + 1
+  })
+  
+  more_loans <- reactive({
+    
+    n <- counter$n
+    
+    if (n > 0) {
+      
+      if(input$fillin){
+        lapply(seq_len(n), function(i) {
+          list(h4(sprintf("Loan #%s", i)),
+               textInput(word_num("loan", i), label = "Name", value = x[[i]]$name),
+               numericInput(word_num("bal", i), label = "Remaining balance", value = x[[i]]$bal, min = 0, step = 20),
+               numericInput(word_num("min_pay", i), label = "Minimum monthly payment", value = x[[i]]$min_pay, min = 10, step = 5),
+               numericInput(word_num("int", i), label = "Interest Rate", value = x[[i]]$int, min = 0, max = 1, step = 0.01),
+               br())
+        })
+      } else {
+        lapply(seq_len(n), function(i) {
+          list(h4(sprintf("Loan #%s", i)),
+               textInput(word_num("loan", i), label = "Name", value = NA),
+               numericInput(word_num("bal", i), label = "Remaining balance", value = NA, min = 0, step = 20),
+               numericInput(word_num("min_pay", i), label = "Minimum monthly payment", value = NA, min = 10, step = 5),
+               numericInput(word_num("int", i), label = "Interest Rate", value = NA, min = 0, max = 1, step = 0.01))
+        })
+      }
+    }
+  })
+  
+  output$more_loans_ui <- renderUI({ more_loans() })
+  
+  loan_list <- eventReactive(input$submit, {
+    loans <- purrr::map(1:(counter$n), function(num){
+      
+      list(name = input[[word_num("loan", num)]], 
+           balance = input[[word_num("bal", num)]],
+           int = input[[word_num("int", num)]],
+           min_pay = input[[word_num("min_pay", num)]])
+    })
+    
+    any_miss <- any(unlist(lapply(loans, function(sub) any(is.na(sub)))))
+    
+    validate(
+      need(!any_miss, "Please fill in missing inputs")
+    )
+    
+    loans
+  })
+  
+  
+  options_plot_data <- eventReactive(input$submit, {
+    #get_payoff_options(loan_list())
+    
+    loan_list <- loan_list()
+    
+    withProgress(message = 'Calculating options\r\n', value = 0, {
+      
+      min_pay <- lapply(loan_list, function(sub_list){
+        sub_list[["min_pay"]]
+      }) %>% unlist %>% sum
+      
+      start_50 <- min_pay - min_pay %% 50 + 50
+      
+      if(abs(min_pay - start_50) < 20) start_50 <- start_50 + 50
+      
+      mo_pay_try <- c(NA, min_pay, seq(start_50, ceiling(start_50+500), by = 50))
+      
+      n <- length(mo_pay_try)
+      
+      summ_sched_df <- data.frame()
+      
+      for(mo_pay in mo_pay_try){
+        
+        mo_pay_display <- ifelse(is.na(mo_pay), min_pay, mo_pay)
+        incProgress(1/n, detail = sprintf("Monthly payment: $%0.2f", mo_pay_display))
+        
+        sched_df <- conduct_schedule_analysis(loan_info_list = loan_list, 
+                                              max_mo_pay = mo_pay, 
+                                              int_tiebreak = "higher balance")
+        
+        summ <- sched_df %>%
+          summarize(`Months to pay off` = max(month),
+                    `Total interest to be paid` = sum(int_pay)) %>%
+          mutate(mo_pay = mo_pay)
+        
+        summ_sched_df <- bind_rows(summ_sched_df, summ)
+      }
+      
+    })
+    
+    summ_sched_df
+    
+  })
+  
+  output$options_plot <- renderPlot({
+    
+    plot_payoff_options(options_plot_data())
+    
+  })
+  
+  mo_pay_choice <- reactive({
+    
+    data <- options_plot_data()
+    
+    min_dat <- data %>%
+      filter(is.na(mo_pay))
+    
+    click <- input$options_click
+    
+    if((click$panelvar1 == "Months to pay off" & click$y > min_dat$`Months to pay off`) |
+       (click$panelvar1 == "Total interest to be paid" & click$y > min_dat$`Total interest to be paid`)){
+      x_val <- NA  
+      
+    } else {
+      x_val <- plyr::round_any(click$x, 50)
+      
+    }
+    
+    if(!is.na(x_val) & x_val < data$mo_pay[2]) x_val <- data$mo_pay[2]
+    
+    x_val
+  })
+  
+  sched <- reactive({
+    
+    req(input$options_click)
+    
+    x_val <- mo_pay_choice()
+    
+    conduct_schedule_analysis(loan_list(), max_mo_pay = x_val)
+    
+  })
+  
+  heading1_str <- eventReactive(input$submit, {
+    "Select a point on either plot to see payment plan:"
+  })
+  
+  output$heading1 <- renderText({
+    heading1_str()
+  })
+  
+  heading2_str <- eventReactive(input$options_click, {
+    "Payment plan"
+  })
+  
+  output$heading2 <- renderText({
+    heading2_str()
+  })
+  
+  output$choice_info <- renderTable({
+    req(input$options_click)
+    
+    data <- options_plot_data()
+    
+    click_val <- mo_pay_choice()
+    
+    values <- data %>% 
+      filter(mo_pay %in% c(NA, click_val)) %>%
+      mutate(`Total interest to be paid` = sprintf("$%0.2f", `Total interest to be paid`),
+             `Months to pay off` = sprintf("%0.0f", `Months to pay off`),
+             mo_pay = sprintf("$%0.2f", mo_pay)) %>%
+      rename(`Monthly payment` = mo_pay) %>%
+      select(`Monthly payment`, `Months to pay off`, `Total interest to be paid`) 
+    
+    values[values$`Monthly payment` == "$NA", "Monthly payment"] = "Minimum"
+    
+    values
+    
+  })
+  
+  output$heading3 <- eventReactive(input$options_click, {
+    "Action items"
+  })
+  
+  output$heading4 <- eventReactive(input$options_click, {
+    "Payments by month under payment plan"
+  })
+  
+  output$schedule_plot <- renderPlot({
+    plot_mo_payments(payment_sched = sched())
+  })
+  
+  output$action_items <- renderText({
+    req(input$options_click)
+    
+    loan_overpay <- sched() %>% 
+      filter(month == 1) %>% 
+      filter(payment > min_pay) 
+    
+    name <- loan_overpay$name
+    total <- loan_overpay$payment
+    amount_overpay <- total - loan_overpay$min_pay
+    
+    sprintf("Call loan servicer for loan '%s' and raise monthly payments 
+            from $%0.2f to $%0.2f", 
+            name, total - amount_overpay, total)
+  })
+  
+}
+
+
+ui <- fluidPage(
+  
+  titlePanel("Student Loan Repayment Calculator"),
+  br(),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      width = 3,
+      
+      tags$head(
+        tags$style(type="text/css", "label.control-label, .form-group.shiny-bound-input{ display: table-cell; text-align: center; vertical-align: middle; } .form-group { display: table-row;}")
+      ),
+      
+      checkboxInput("fillin", "Fill in example loan figures?", value = TRUE),
+      uiOutput("more_loans_ui"),
+      actionButton("add_loan", label = "", icon = icon("plus-square")),
+      actionButton("minus_loan", label = "", icon = icon("minus-square")),
+      actionButton("submit", "Show me my options")
+      
+    ),
+    
+    mainPanel(
+      
+      h5(textOutput("heading1")),
+      plotOutput(outputId = "options_plot", click = "options_click"),
+      
+      h4(textOutput("heading2")),
+      tableOutput(outputId = "choice_info"),
+      
+      h4(textOutput("heading3")),
+      textOutput("action_items"),
+      
+      h4(textOutput("heading4")), 
+      plotOutput(outputId = "schedule_plot")
+      
+    )
+  )
+)
+
+shinyApp(
+  ui = ui,
+  server = server
+)
+
---
+++ loans_calc/source.R
@@ -0,0 +1,286 @@
+calc_mo_pay = function(start_balance, int, payment){
+  
+  int_pay = start_balance*int/12
+  prin_pay = payment - int_pay
+  total_pay = int_pay + prin_pay
+  
+  new_balance = start_balance - prin_pay
+  
+  if(new_balance < 0){
+    payment = start_balance + int_pay
+    new_balance = 0
+  }
+  
+  data.frame("payment" = payment, "old_balance" = start_balance, 
+             "new_balance" = new_balance, "int_pay" = int_pay)
+  
+}
+
+conduct_schedule_analysis = function(loan_info_list, max_mo_pay = NA, 
+                                     int_tiebreak = "higher balance"){
+  
+  loan_info_df = purrr::map_df(loan_info_list, function(sublist){ 
+    data.frame(sublist, stringsAsFactors = F)
+  })
+  
+  if(int_tiebreak == "higher balance"){
+    loan_df_sort = loan_info_df %>%
+      arrange(desc(int), desc(balance))
+  } else if(int_tiebreak == "lower balance"){
+    loan_df_sort = loan_info_df %>%
+      arrange(desc(int), balance)
+  } else {
+    loan_df_sort = loan_info_df %>%
+      arrange(desc(int), desc(balance))
+    warning("No proper tiebreaker defined")
+  }
+  
+  if(!is.na(max_mo_pay) && max_mo_pay < sum(loan_df_sort$min_pay)){
+    stop("you have entered a maximum monthly payment that is ",
+         "lower than the total minimum loan payments")
+  }
+  
+  
+  orig_order <- loan_df_sort$name
+  
+  month = 1
+  
+  total_balance = sum(loan_df_sort$balance)
+  
+  all_payment_df = NULL
+  
+  while(total_balance > 0){
+    
+    if(!is.na(max_mo_pay)){
+      overpay_loan = loan_df_sort$name[1]
+      if(nrow(loan_df_sort) > 1){
+        non_overpay_loans = loan_df_sort$name[-1]
+        next_overpay = loan_df_sort$name[2]
+      } else {
+        non_overpay_loans = NULL
+        next_overpay = NULL
+      }
+    } else {
+      overpay_loan = NULL
+      next_overpay = NULL
+      non_overpay_loans = loan_df_sort$name
+    }
+    
+    if(length(non_overpay_loans)>0){
+      min_pay_df = purrr::map_df(non_overpay_loans, function(loan_name){
+        
+        loan_row = loan_df_sort %>% filter(name == loan_name)
+        
+        calc_mo_pay(start_balance = loan_row$balance, 
+                    int = loan_row$int, payment = loan_row$min_pay) %>%
+          mutate(name = loan_name, type = "min_payment",
+                 min_pay = min(loan_row$min_pay, payment),
+                 month = month)
+      })
+      
+    } else {
+      
+      min_pay_df <- NULL
+      
+    }
+    
+    total_min_pay = sum(min_pay_df$payment)
+    
+    redirect_overpay = NULL
+    
+    if(length(overpay_loan)>0){
+      overpay = max_mo_pay - total_min_pay 
+      
+      if(overpay < 0){
+        overpay_df = NULL
+      } else {
+        loan_row = loan_df_sort %>% filter(name == overpay_loan)
+        overpay_df = calc_mo_pay(start_balance = loan_row$balance, 
+                                 int = loan_row$int, payment = overpay) %>%
+          mutate(month = month,
+                 name = overpay_loan, 
+                 min_pay = min(loan_row$min_pay, payment),
+                 type = "overpay")
+        
+        if(overpay_df$payment < overpay){
+          redirect_overpay = overpay - overpay_df$payment
+        }
+      }
+      
+      payment_df = bind_rows(min_pay_df, overpay_df) %>% 
+        mutate(month = month)
+      
+      # if leftover extra pay from overpay loan 
+      if(length(redirect_overpay)>0 & length(next_overpay)>0){
+        orig_payment = payment_df %>%
+          filter(name == next_overpay) %>%
+          pull(payment)
+        
+        
+        loan_row = loan_df_sort %>% filter(name == next_overpay)
+        
+        redo_pay = calc_mo_pay(start_balance = loan_row$balance, 
+                               int = loan_row$int, payment = orig_payment + redirect_overpay) %>%
+          mutate(name = next_overpay, type = "min_payment",
+                 min_pay = min(orig_payment + redirect_overpay, loan_row$min_pay),
+                 month = month)
+        
+        payment_df = payment_df %>%
+          filter(name != next_overpay) %>%
+          bind_rows(redo_pay)
+        
+      }
+      
+    } else {
+      payment_df = min_pay_df  
+    }
+    
+    all_payment_df = bind_rows(all_payment_df, payment_df)
+    
+    update_loan_df = left_join(loan_df_sort, payment_df %>% select(name, new_balance), by = "name")
+    
+    loan_df_sort = update_loan_df %>% 
+      mutate(balance = new_balance) %>%
+      select(-new_balance) %>%
+      filter(balance > 0)
+    
+    total_balance = sum(loan_df_sort$balance)
+    
+    month = month + 1
+    
+  }
+  
+  all_payment_df %>%
+    mutate(name = factor(name, levels = orig_order)) 
+}
+
+plot_mo_payments = function(payment_sched){
+  
+  min_pay = payment_sched %>% 
+    group_by(month) %>% 
+    summarize(min_pay_mo = sum(min_pay))
+  
+  cols = c("#11CEC4", "#218ED2", "#3250D6", "#6A43DA", 
+           "#B455DE", "#E267D2", "#E67AA9", "#EB8EBE")
+  
+  cols_use_seq = seq(1, 8, by = floor(8/length(unique(payment_sched$name))))
+  
+  cols_use = cols[cols_use_seq]
+  
+  ggplot(payment_sched, aes(x = month, y = payment)) +
+    geom_bar(stat = "identity", color = "white", aes(fill = name), alpha = .6) +
+    geom_line(data = min_pay, aes(y = min_pay_mo, color = "Minimum monthly\n payments"), size = 1) +
+    ylab("Total Monthly Payment") +
+    scale_fill_manual("", values = cols_use) +
+    scale_x_continuous("Month from now", breaks = seq(12, max(payment_sched$month), by = 12)) +
+    ggthemes::scale_color_gdocs()
+  
+}
+
+
+plot_mo_payments2 = function(payment_sched){
+  
+  min_pay = payment_sched %>%
+    group_by(month) %>%
+    summarize(min_pay_mo = sum(min_pay))
+  
+  pay_order <- levels(payment_sched$name)
+  
+  
+  payment_sched <- payment_sched %>%
+    ungroup() %>%
+    mutate(overpay = payment - min_pay) %>%
+    gather(group, pay_amount, min_pay, overpay) %>%
+    filter(!(group == "overpay" & pay_amount == 0)) %>%
+   # mutate(group = factor(group, levels = c("min_pay", "overpay"), 
+  #                        labels = c("minimum payment", "overpayment"))) %>%
+   arrange(name, group) %>%
+   mutate(name2 = sprintf("%s %s", name, group))
+  
+ return(payment_sched)
+  
+  cols = c("#11CEC4", "#218ED2", "#3250D6", "#6A43DA", 
+           "#B455DE", "#E267D2", "#E67AA9", "#EB8EBE")
+  
+  cols_use_seq = seq(1, 8, by = floor(8/length(unique(payment_sched$name))))
+  
+  cols_use = cols[cols_use_seq]
+
+  ggplot(payment_sched, aes(x = month, y = pay_amount, group = name2)) +
+    geom_bar(stat = "identity", aes(fill = name, alpha = group), color = "white") +
+    #  geom_line(data = min_pay, aes(y = min_pay_mo, color = "Minimum monthly\n payments"), size = 1) +
+    ylab("Total Monthly Payment") +
+    scale_fill_manual("", values = cols_use) +
+    scale_alpha_manual("", values = c(1, .6)) + 
+    theme_minimal() +
+    scale_x_continuous("Month from now", breaks = seq(12, max(payment_sched$month), by = 12))
+  
+}
+
+get_payoff_options <- function(loan_info_list){ 
+  
+  min_pay <- lapply(loan_info_list, function(sub_list){
+    sub_list[["min_pay"]]
+  }) %>% unlist %>% sum
+  
+  start_50 <- min_pay - min_pay %% 50 + 50
+  
+  if(abs(min_pay - start_50) < 20) start_50 <- start_50 + 50
+  
+  mo_pay_try <- c(NA, min_pay, seq(start_50, ceiling(start_50+500), by = 50))
+  
+  all_payoff_options <- purrr::map_df(mo_pay_try, function(mo_pay){
+    
+    sched_df <- conduct_schedule_analysis(loan_info_list = loan_info_list, 
+                                          max_mo_pay = mo_pay, 
+                                          int_tiebreak = "higher balance")
+    
+    sched_df %>%
+      summarize(`Months to pay off` = max(month),
+                `Total interest to be paid` = sum(int_pay)) %>%
+      mutate(mo_pay = mo_pay)
+  })
+  
+}
+
+plot_payoff_options <- function(payoff_options){
+  
+  maxval <- max(payoff_options$mo_pay, na.rm = T)
+  
+  curr_pay <- payoff_options$mo_pay[2]
+  
+  all_gather <- payoff_options %>% 
+    filter(!is.na(mo_pay)) %>%
+    gather(group, value, -mo_pay) %>%
+    mutate(value_lab = case_when(group == "Months to pay off" ~ as.character(value),
+                                 TRUE ~ sprintf("$%0.2f", value)))
+  
+  start_x <- payoff_options$mo_pay[2]
+  put_text <- start_x + .5*(maxval - start_x)
+  
+  baremin_gather <- payoff_options %>%
+    filter(is.na(mo_pay)) %>%
+    gather(group, value, -mo_pay) %>%
+    mutate(value_lab = round(value),
+           padding = .07*value,
+           nudge_x = .05*(maxval - start_x))
+  
+  ggplot(all_gather, aes(x = mo_pay, y = value)) + 
+    facet_wrap(~group, scales = "free_y", ncol = 2) +
+    geom_line() +
+    geom_point() +
+    geom_text(data = baremin_gather, position = "dodge",
+              aes(x = put_text, y = value + padding, 
+                  label = "Making no overpayments"), size = 4, col = "red") +
+    geom_text(data = baremin_gather, position = "dodge",
+              aes(x = put_text, y = value + padding/3), 
+                  label = sprintf("(Currently $%0.2f)", curr_pay), size = 3, col = "red") +
+    geom_hline(data = baremin_gather, aes(yintercept = value), col = "red") +
+    geom_hline(aes(yintercept = 0), col = NA) +
+    labs(x = "Total monthly payments", y = "") +
+    scale_x_continuous(labels = scales::dollar_format(prefix="$"), lim = c(start_x, maxval)) 
+}
+
+
+
+
